b7654f1409624db4dac827bcb34ab036040b3ce9
Emil <emil@personalkollen.se>
2024-03-28 14:58:20 +0100

    Enable docker

--- hosts/public-www/configuration.nix
+++ hosts/public-www/configuration.nix
@@ -23,16 +23,15 @@
     }
   ];
   networking.firewall.interfaces."ens18".allowedTCPPorts = [80 443];
-
+  networking.hostName = "public-www";
   networking.defaultGateway = "10.10.10.1";
   networking.nameservers = ["1.1.1.1"];
+  networking.networkmanager.enable = true;
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.trusted-users = ["root" "emil"];
 
-  networking.hostName = "public-www";
-
-  networking.networkmanager.enable = true;
+  virtualisation.docker.enable = true;
 
   time.timeZone = "Europe/Stockholm";
 
@@ -64,6 +63,12 @@
     openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+SY+kqLyZDhwcUgS8E+B1jqvKJBI4YcSjehxqJ45AkgqRw/lgTAWhEHzjN/SN0DYt04aSr6flwe2BSAr96MSKJ+QN4RhqjyQbcitaw04Gc5/A09acjsY1a9Hqf3ofmK2OfjacbvN2XaHBuyMixI3Z8t2pm615z+S5mBVxQJHdnCuRF+QH9gZKBPKHkzdIqTBGL+XBCXJLTXCdR8F9yKyIYUTgJdxUhiHpxC98oI38ITjDIyBCmt3WNGXHeDtai0PqMckYcb9xKsM9qXE/1CCrcaBk4pw3yOz4wW3xCHNHKUBoBa9w5E7cTxv5ADQdMfgTQFz2eHe8I0uVwSxR368k9IBPkzqSjEhVIMHEsLS8GyN/oY5UKf1OowmYk+0dJscsDLJL6TMLISIzF5sxp6QufOWXwdMzIkpxIXzTNLjPCvxnQtb7Mc2YbYeBPZV0A/pitITWzfxGulK7SLQ4RCwMSm4Yi+2Pc96dzLggwUZSZuNXqm/bEuaIPcU+t41eM2E="];
   };
 
+  users.users.public = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = ["docker"];
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
@@ -73,6 +78,7 @@
     direnv
     bat
     caddy
+    docker-compose
   ];
   system.stateVersion = "23.11"; # Did you read the comment?
 }
