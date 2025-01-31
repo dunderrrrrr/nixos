44ab48fc091872f85f5c507d34244658af44b222
dunderrrrrr <emil.bjorkroth@gmail.com>
2025-01-31 08:51:32 +0100

    work: move jujutsu config

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -2,6 +2,7 @@
   imports = [
     ./programs/vscode.nix
     ./programs/git.nix
+    ./programs/jujutsu.nix
     ./programs/firefox.nix
     ./programs/kitty.nix
   ];
@@ -9,28 +10,4 @@
   home.stateVersion = "23.11";
 
   programs.direnv.enable = true;
-
-  programs.jujutsu = {
-    enable = true;
-    settings = {
-      user = {
-        email = "emil@personalkollen.se";
-        name = "Emil Björkroth";
-      };
-      ui = {
-        default-command = "log"; # Allow running `jj` to see the log without a warning
-        conflict-marker-style = "git";
-        pager = "delta";
-        diff.format = "git";
-        default-description = ''
-          JJ: If applied, this commit will...
-
-          JJ: Explain why this change is being made
-
-          JJ: Provide links to any relevant tickets, articles or other resources
-
-        '';
-      };
-    };
-  };
 }
