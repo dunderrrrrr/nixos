bc203682abb427554b710097a413bcbc624704ce
dunderrrrrr <emil.bjorkroth@gmail.com>
2025-03-03 16:31:59 +0100

    work: vscode options was renamed
    evaluation warning: emil profile: The option `programs.vscode.globalSnippets' defined in `/nix/store/r9q9dqmn18bvnw72nyq30zy10bhpmfya-source/hosts/work/programs/vscode.nix' has been renamed to `programs.vscode.profiles.default.globalSnippets'.
    evaluation warning: emil profile: The option `programs.vscode.keybindings' defined in `/nix/store/r9q9dqmn18bvnw72nyq30zy10bhpmfya-source/hosts/work/programs/vscode.nix' has been renamed to `programs.vscode.profiles.default.keybindings'.
    evaluation warning: emil profile: The option `programs.vscode.userSettings' defined in `/nix/store/r9q9dqmn18bvnw72nyq30zy10bhpmfya-source/hosts/work/programs/vscode.nix' has been renamed to `programs.vscode.profiles.default.userSettings'.

--- hosts/work/programs/vscode.nix
+++ hosts/work/programs/vscode.nix
@@ -1,81 +1,83 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    userSettings = {
-      "workbench.colorTheme" = "Monokai";
-      "workbench.iconTheme" = "vscode-icons";
-      "workbench.list.smoothScrolling" = true;
+    profiles.default = {
+      userSettings = {
+        "workbench.colorTheme" = "Monokai";
+        "workbench.iconTheme" = "vscode-icons";
+        "workbench.list.smoothScrolling" = true;
 
-      "vsicons.presets.foldersAllDefaultIcons" = true;
-      "explorer.confirmDragAndDrop" = false;
+        "vsicons.presets.foldersAllDefaultIcons" = true;
+        "explorer.confirmDragAndDrop" = false;
 
-      "editor.formatOnSave" = true;
-      "editor.stickyScroll.enabled" = true;
-      "editor.minimap.showSlider" = true;
-      "editor.multiCursorLimit" = 99999999;
+        "editor.formatOnSave" = true;
+        "editor.stickyScroll.enabled" = true;
+        "editor.minimap.showSlider" = true;
+        "editor.multiCursorLimit" = 99999999;
 
-      "editor.smoothScrolling" = true;
-      "editor.cursorBlinking" = "smooth";
-      "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.smoothScrolling" = true;
+        "editor.cursorBlinking" = "smooth";
+        "editor.cursorSmoothCaretAnimation" = "on";
 
-      "terminal.integrated.tabs.enabled" = false;
-      "terminal.integrated.altClickMovesCursor" = false;
-      "terminal.integrated.scrollback" = 9999999999;
-      "terminal.integrated.smoothScrolling" = true;
-      "terminal.integrated.cursorBlinking" = true;
+        "terminal.integrated.tabs.enabled" = false;
+        "terminal.integrated.altClickMovesCursor" = false;
+        "terminal.integrated.scrollback" = 9999999999;
+        "terminal.integrated.smoothScrolling" = true;
+        "terminal.integrated.cursorBlinking" = true;
 
-      "window.titleBarStyle" = "custom";
-      "window.customMenuBarAltFocus" = false;
-      "window.enableMenuBarMneMonics" = false;
-      "window.menuBarVisibility" = "visible";
-      "window.zoomLevel" = -1;
-    };
-    keybindings = [
-      {
-        key = "ctrl+shift+[minus]";
-        command = "workbench.action.focusActiveEditorGroup";
-      }
-    ];
-    globalSnippets = {
-      pin = {
-        prefix = ["pin"];
-        description = "Inserts full pin word";
-        body = ["personal_identification_number"];
+        "window.titleBarStyle" = "custom";
+        "window.customMenuBarAltFocus" = false;
+        "window.enableMenuBarMneMonics" = false;
+        "window.menuBarVisibility" = "visible";
+        "window.zoomLevel" = -1;
       };
-      type_checking = {
-        prefix = ["type_checking"];
-        description = "Inserts TYPE_CHECKING block";
-        body = [
-          "from __future__ import annotations"
-          "from typing import TYPE_CHECKING"
-          ""
-          "if TYPE_CHECKING:"
-          "    "
-        ];
-      };
-      template_script = {
-        prefix = ["template_script"];
-        description = "Inserts a simple PK script starter template";
-        body = [
-          "import click"
-          ""
-          "from pk.utils.cli import command"
-          ""
-          "@command()"
-          "@click.option(\"--commit\", is_flag=True, default=False)"
-          "def main(commit):"
-          ""
-          "    assert commit"
-          "if __name__ == \"__main__\":"
-          "    main()"
-        ];
-      };
-      breakpoint = {
-        prefix = ["bkp"];
-        description = "Inserts breakpoint()";
-        body = [
-          "breakpoint()"
-        ];
+      keybindings = [
+        {
+          key = "ctrl+shift+[minus]";
+          command = "workbench.action.focusActiveEditorGroup";
+        }
+      ];
+      globalSnippets = {
+        pin = {
+          prefix = ["pin"];
+          description = "Inserts full pin word";
+          body = ["personal_identification_number"];
+        };
+        type_checking = {
+          prefix = ["type_checking"];
+          description = "Inserts TYPE_CHECKING block";
+          body = [
+            "from __future__ import annotations"
+            "from typing import TYPE_CHECKING"
+            ""
+            "if TYPE_CHECKING:"
+            "    "
+          ];
+        };
+        template_script = {
+          prefix = ["template_script"];
+          description = "Inserts a simple PK script starter template";
+          body = [
+            "import click"
+            ""
+            "from pk.utils.cli import command"
+            ""
+            "@command()"
+            "@click.option(\"--commit\", is_flag=True, default=False)"
+            "def main(commit):"
+            ""
+            "    assert commit"
+            "if __name__ == \"__main__\":"
+            "    main()"
+          ];
+        };
+        breakpoint = {
+          prefix = ["bkp"];
+          description = "Inserts breakpoint()";
+          body = [
+            "breakpoint()"
+          ];
+        };
       };
     };
   };
