3d6f3552c8b18a114f38ad2c3f0c1c990a96d37d
dunderrrrrr <emil.bjorkroth@gmail.com>
2025-06-11 08:16:22 +0200

    fwk: jujutsu option renamed diff-formatter

--- hosts/fwk/programs/jujutsu.nix
+++ hosts/fwk/programs/jujutsu.nix
@@ -10,7 +10,7 @@
         default-command = "log"; # Allow running `jj` to see the log without a warning
         conflict-marker-style = "git";
         pager = "delta";
-        diff.format = "git";
+        diff-formatter = "git";
       };
       template-aliases.default_commit_description = ''
         "JJ: If applied, this commit will...
