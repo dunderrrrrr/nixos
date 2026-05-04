3e4e2b835bfbbf6e95822cf617cc6bba3b062f0b
dunderrrrrr <emil.bjorkroth@gmail.com>
2026-05-04 20:25:27 +0200

    nixos-blocket: cleanups

--- hosts/nixos-blocket/configuration.nix
+++ hosts/nixos-blocket/configuration.nix
@@ -24,13 +24,61 @@ in {
     443
   ];
 
-  services.openssh = {
-    enable = true;
-    ports = [22228];
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
+  environment.systemPackages = with pkgs; [
+    jq
+    git
+    caddy
+    screen
+    docker-compose
+  ];
+
+  services = {
+    openssh = {
+      enable = true;
+      ports = [22228];
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+
+    journald.extraConfig = ''
+      SystemMaxUse=300M
+      SystemMaxFileSize=40M
+      MaxRetentionSec=4day
+    '';
+
+    vector = {
+      enable = true;
+      settings = {
+        sources.caddy_logs = {
+          type = "file";
+          include = ["/var/log/caddy/*.log"];
+        };
+        transforms.parse_caddy = {
+          type = "remap";
+          inputs = ["caddy_logs"];
+          source = ''
+            . = parse_json!(.message)
+            ._msg = .msg
+               .country = string!(.request.headers."Cf-Ipcountry"[0])
+               .request_path = split!(.request.uri, "?")[0]
+          '';
+        };
+        sinks.victorialogs = {
+          type = "elasticsearch";
+          inputs = ["parse_caddy"];
+          endpoints = ["http://127.0.0.1:9428/insert/elasticsearch/"];
+          mode = "bulk";
+          api_version = "v8";
+          request.headers = {
+            AccountID = "0";
+            ProjectID = "0";
+            VL-Stream-Fields = "host,logger";
+          };
+        };
+      };
     };
   };
 
@@ -46,12 +94,6 @@ in {
   ];
 
   console.keyMap = "sv-latin1";
-
-  services.journald.extraConfig = ''
-    SystemMaxUse=300M
-    SystemMaxFileSize=40M
-    MaxRetentionSec=4day
-  '';
 
   users = {
     users.emil = {
@@ -77,94 +119,52 @@ in {
     groups.vector = {};
   };
 
-  environment.systemPackages = with pkgs; [
-    jq
-    git
-    caddy
-    screen
-    docker-compose
-  ];
-
-  systemd.services.api = {
-    enable = true;
-    description = "Gunicorn instance to serve the api";
-    after = ["network.target"];
-    serviceConfig = {
-      User = "emil";
-      WorkingDirectory = blocketapiProjectRoot;
-      ExecStart = "/home/emil/projects/blocket-api.se/.venv/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker api:app -b 127.0.0.1:8000 --access-logfile - --error-logfile - --log-level info";
+  systemd.services = {
+    api = {
+      enable = true;
+      description = "Gunicorn instance to serve the api";
+      after = ["network.target"];
+      serviceConfig = {
+        User = "emil";
+        WorkingDirectory = blocketapiProjectRoot;
+        ExecStart = "/home/emil/projects/blocket-api.se/.venv/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker api:app -b 127.0.0.1:8000 --access-logfile - --error-logfile - --log-level info";
+      };
+      environment = {
+        PATH = lib.mkForce "/home/emil/projects/blocket-api.se/.venv/bin/";
+      };
+      wantedBy = ["multi-user.target"];
     };
-    environment = {
-      PATH = lib.mkForce "/home/emil/projects/blocket-api.se/.venv/bin/";
-    };
-    wantedBy = ["multi-user.target"];
   };
-
   systemd.tmpfiles.rules = [
     "d /var/log/caddy 0755 caddy users -"
     "d /var/lib/victorialogs 0755 root root -"
     "d /var/lib/grafana 0755 472 472 -"
   ];
 
-  virtualisation = {
-    oci-containers = {
-      containers = {
-        victorialogs = {
-          image = "victoriametrics/victoria-logs:latest";
-          ports = ["127.0.0.1:9428:9428"];
-          volumes = [
-            "/var/lib/victorialogs:/vlogs"
-          ];
-          cmd = [
-            "-storageDataPath=/vlogs"
-            "-retentionPeriod=4w"
-          ];
-        };
-        grafana = {
-          image = "grafana/grafana:latest";
-          ports = ["127.0.0.1:3000:3000"];
-          extraOptions = ["--network=host"];
-          volumes = [
-            "/var/lib/grafana:/var/lib/grafana"
-          ];
-          environment = {
-            GF_SERVER_ROOT_URL = "https://logs.blocket-api.se";
-            GF_SERVER_DOMAIN = "logs.blocket-api.se";
-            GF_INSTALL_PLUGINS = "victoriametrics-logs-datasource";
-          };
-        };
-      };
+  virtualisation.oci-containers.containers = {
+    victorialogs = {
+      image = "victoriametrics/victoria-logs:latest";
+      ports = ["127.0.0.1:9428:9428"];
+      volumes = [
+        "/var/lib/victorialogs:/vlogs"
+      ];
+      cmd = [
+        "-storageDataPath=/vlogs"
+        "-retentionPeriod=4w"
+      ];
     };
-  };
 
-  services.vector = {
-    enable = true;
-    settings = {
-      sources.caddy_logs = {
-        type = "file";
-        include = ["/var/log/caddy/*.log"];
-      };
-      transforms.parse_caddy = {
-        type = "remap";
-        inputs = ["caddy_logs"];
-        source = ''
-          . = parse_json!(.message)
-          ._msg = .msg
-             .country = string!(.request.headers."Cf-Ipcountry"[0])
-             .request_path = split!(.request.uri, "?")[0]
-        '';
-      };
-      sinks.victorialogs = {
-        type = "elasticsearch";
-        inputs = ["parse_caddy"];
-        endpoints = ["http://127.0.0.1:9428/insert/elasticsearch/"];
-        mode = "bulk";
-        api_version = "v8";
-        request.headers = {
-          AccountID = "0";
-          ProjectID = "0";
-          VL-Stream-Fields = "host,logger";
-        };
+    grafana = {
+      image = "grafana/grafana:latest";
+      ports = ["127.0.0.1:3000:3000"];
+      extraOptions = ["--network=host"];
+      volumes = [
+        "/var/lib/grafana:/var/lib/grafana"
+      ];
+      environment = {
+        GF_SERVER_ROOT_URL = "https://logs.blocket-api.se";
+        GF_SERVER_DOMAIN = "logs.blocket-api.se";
+        GF_INSTALL_PLUGINS = "victoriametrics-logs-datasource";
       };
     };
   };
