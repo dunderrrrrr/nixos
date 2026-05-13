cfc86bb18ed00a57c6cd1727056c6141b96b74dd
dunderrrrrr <emil.bjorkroth@gmail.com>
2026-05-13 21:32:46 +0200

    nixos-public: add staeder user

--- hosts/nixos-public/users.nix
+++ hosts/nixos-public/users.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   constants = import ../_shared_configs/constants.nix;
-  serviceUsers = ["hotels" "swarje" "domanfluff" "wcwp"];
+  serviceUsers = ["hotels" "swarje" "domanfluff" "wcwp" "staeder"];
   mkServiceUser = name: {
     isSystemUser = true;
     group = name;
