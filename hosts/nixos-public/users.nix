04cfedbad46e64c5f4f9b1babb4e78263a3f7adb
dunderrrrrr <emil.bjorkroth@proton.me>
2026-08-07 19:59:35 +0200

    nixos-public: add ha-backups user

--- hosts/nixos-public/users.nix
+++ hosts/nixos-public/users.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   constants = import ../_shared_configs/constants.nix;
-  serviceUsers = ["hotels" "swarje" "domanfluff" "wcwp" "staeder" "tagpuls"];
+  serviceUsers = ["hotels" "swarje" "domanfluff" "wcwp" "staeder" "tagpuls" "ha-backups"];
   mkServiceUser = name: {
     isSystemUser = true;
     group = name;
